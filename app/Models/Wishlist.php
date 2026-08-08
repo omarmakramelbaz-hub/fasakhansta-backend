896679e2a8616213ecf1a402895bcd15c729117b
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-08 18:32:12 +0300

    Add product relation to wishlist

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\HasMedia;
-use Illuminate\Support\Facades\App;
 
 class Wishlist extends Model implements HasMedia
 {
@@ -14,11 +13,19 @@ class Wishlist extends Model implements HasMedia
     use HasFactory;
 
     protected $guarded = [];
-    public function resturant() {
-       return $this->belongsTo(\App\Models\Resturant::class,'resturant_id');
+
+    public function resturant()
+    {
+        return $this->belongsTo(Resturant::class, 'resturant_id');
     }
-    
-     public function user() {
-       return $this->belongsTo(\App\Models\User::class,'user_id');
+
+    public function resturantProduct()
+    {
+        return $this->belongsTo(ResturantProduct::class, 'resturant_product_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
