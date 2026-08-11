dbf665161b5441e3f2288daa74889a95c0d915ee
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-11 15:15:50 +0300

    Fix FCM project ID to match Firebase service account

--- config/services.php
+++ config/services.php
@@ -9,8 +9,8 @@ return [
     |
     | This file is for storing the credentials for third party services such
     | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
+    | location for this type of information, allowing packages to have a
+    | conventional file to locate this information.
     |
     */
 
@@ -29,8 +29,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+
     'fcm' => [
-        'project_id' => 'fasakhaninjatest',
+        'project_id' => env('FCM_PROJECT_ID', 'fasakha-16ecd'),
     ],
 
     'google' => [
