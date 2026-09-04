1c213785549e7adad788474d3f1eabc00d547aa6
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-04 21:07:51 +0300

    Configure Meta Conversions API

--- __config/services.php
+++ __config/services.php
@@ -33,4 +33,22 @@ return [
         'project_id' => 'fasakhaninjatest',
     ],
 
+    'meta_conversions' => [
+        'enabled' => env('META_CONVERSIONS_ENABLED', false),
+        'dataset_id' => env('META_DATASET_ID'),
+        'access_token' => env('META_ACCESS_TOKEN'),
+        'api_version' => env('META_GRAPH_API_VERSION', 'v24.0'),
+        'currency' => env('META_CURRENCY', 'EGP'),
+        'app_platform' => env('META_APP_PLATFORM', 'android'),
+        'android_package' => env('META_ANDROID_PACKAGE', 'com.smartvision.faskhanista'),
+        'ios_bundle_id' => env('META_IOS_BUNDLE_ID', 'com.faskhaninja.clients'),
+        'app_version' => env('META_APP_VERSION', ''),
+        'app_build' => env('META_APP_BUILD', ''),
+        'app_locale' => env('META_APP_LOCALE', 'ar_EG'),
+        'app_timezone' => env('META_APP_TIMEZONE', 'Africa/Cairo'),
+        'advertiser_tracking_enabled' => env('META_ADVERTISER_TRACKING_ENABLED', false),
+        'application_tracking_enabled' => env('META_APPLICATION_TRACKING_ENABLED', false),
+        'test_event_code' => env('META_TEST_EVENT_CODE'),
+    ],
+
 ];
